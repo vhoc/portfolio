9d4f0010dfb1a7e2d2027923a429588a022be848
Victor Olvera <vhocar@gmail.com>
2021-12-12 12:34:22 -0700

    fixed details

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
 
         <>
             <section className="py-3">
-                <div className="d-flex justify-content-center about flex-column mt-3">
+                <div className="d-flex about flex-column align-items-center mt-3">
                     <div>About me</div>
                     <div className="short-description">Self-taught IT expert and programmer</div>
                     <div className="brief px-5 m-2">Technology and computers have been my passion from a young age. Right now I'm in the process of a career turn from a more hardware and technical aspect of Information Technologies into the world of web and software development. With previous notions of PHP, HTML, CSS and Javascript I started to learn and practice the Laravel Framework, among other related tools, and more recently: Javascript ES6+, React, C# language and Git. I've also made and contributed to several Wordpress websites.</div>
--- src/components/portfolio-components/Contact.js
+++ src/components/portfolio-components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
 
             <h1>Contact</h1>
             {/*I know, yes, this is an iframe. I'll use a proper form as soon as I finish building it*/}
-            <iframe className='col-12' src='https://waxyform.com/app/app/form?id=43&b=0' height='700px'>
+            <iframe className='col-12 col-sm-10 col-md-7 col-lg-6 col-xl-4' src='https://waxyform.com/app/app/form?id=43&b=0' height='700px'>
                 
             </iframe>
             
